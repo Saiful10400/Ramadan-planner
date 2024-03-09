d97255d25e0b59503c9c6e7f2a6765dc1bacad12
saiful10400 <saifulislam10400i5@gmail.com>
2024-03-09 16:35:26 +0600

    authentication page initialization done

--- src/RouterProvider.jsx
+++ src/RouterProvider.jsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./components/Root";
+ 
+ 
+import Authentication from "./components/childrens/Authentication";
 
 export const router=createBrowserRouter([
     {
         path:"/",
-        element:<Root></Root>
+        element:<Root></Root>,
+        children:[
+            {
+                path:"/register",
+                element:<Authentication></Authentication>
+            },
+            {
+                path:"/login",
+                element:<Authentication></Authentication>
+            }
+        ]
     }
 ])
---
+++ src/components/childrens/Authentication.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./authentication.css"
+
+const Authentication = () => {
+    return (
+        <div id='authContainer' className='min-h-screen flex justify-center items-center'>
+          <div id='loginContainer' className='text-white min-h-[500px] border min-w-[400px]'>
+            <h1>main authContainer</h1>
+          </div>
+        </div>
+    );
+};
+
+export default Authentication;
